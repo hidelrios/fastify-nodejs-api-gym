b99fa29cc51de120a344107d32b32431d1f0cb4b
hidelrios <hidels.rios@gmail.com>
2024-10-02 06:09:50 -0300

    multiplas alteracoes

--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -1,9 +1,13 @@
-import { authenticate } from "./controllers/authenticate";
-import { register } from "./controllers/register";
+import { authenticate } from "./controllers/users/authenticate";
+import { profile } from "./controllers/users/profile";
+import { register } from "./controllers/users/register";
 import { FastifyInstance } from "fastify";
+import { verifyJwt } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
 
-  app.post("/sessios", authenticate);
+  app.post("/sessions", authenticate);
+  /** Authenticated */
+  app.get("/me", { onRequest: [verifyJwt] }, profile);
 }
--- 03-api-solid/src/use-cases/factories/make-authenticate-use-case.ts
+++ 03-api-solid/src/use-cases/factories/make-authenticate-use-case.ts
@@ -1,8 +1,8 @@
-import { PrismaUserRepository } from "@/repositories/prisma/prisma-users-repository";
+import { PrismaUsersRepository } from "../../repositories/prisma/prisma-users-repository";
 import { AuthenticateUseCase } from "../authenticate";
 
 export function makeAuthenticateUseCase() {
-  const usersRepository = new PrismaUserRepository();
+  const usersRepository = new PrismaUsersRepository();
   const authenticateUseCase = new AuthenticateUseCase(usersRepository);
 
   return authenticateUseCase;
