d74989b0d4ec97e92e7ce0f45838d90ecef5f880
hidelrios <hidels.rios@gmail.com>
2024-10-09 22:13:15 -0300

    finalizando projeto

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
+import { refresh } from "./http/controllers/users/refresh";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
 import { checkInsRoutes } from "./http/controllers/check-ins/routes";
@@ -10,11 +12,20 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
 });
 
+app.register(fastifyCookie);
 app.register(usersRoutes);
 app.register(gymsRoutes);
 app.register(checkInsRoutes);
+app.patch("/token/refresh", refresh);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
